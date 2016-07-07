1c55ef14b7b82e0b249bb1b047328fe5d9cb88b1
mattlaguardia <matthewlaguardia@gmail.com>
2016-07-07 16:23:32 -0700

    adds resource to branch

--- starter-code/frontend/js/app.js
+++ starter-code/frontend/js/app.js
@@ -1,1 +1,1 @@
-angular.module("InfamousCriminalsApp", []);
+angular.module("InfamousCriminalsApp", ['ngResource']);
--- starter-code/frontend/js/criminalsController.js
+++ starter-code/frontend/js/criminalsController.js
@@ -1,46 +1,62 @@
-angular.module("InfamousCriminalsApp", [])
-  .controller("CriminalsController", CriminalsController);
+console.log("Linked!")
 
-  CriminalsController.$inject = ["$http"];
+var app = angular.module("InfamousCriminalsApp");
 
-  function CriminalsController($http) {
-    var self = this
-    self.newCriminal = {};
-    self.addCriminal = addCriminal;
-    self.getCriminals = getCriminals;
-    self.deleteCriminal = deleteCriminal;
-    self.all = [];
 
-    function addCriminal() {
-      $http.post("http://localhost:3000/criminals", self.newCriminal)
-        .then(function(data){
-          self.all = newCriminal.data.criminals;
-        })
-        self.all.push(this.newCriminal);
-        self.newCriminal = {};
+app.factory("Criminal", function($resource){
+  return $resource("http://localhost:3000/criminals/:id", null, {
+    query: {
+      method: 'GET',
+      isArray: false
     }
+  })
+});
 
-    function deleteCriminal(criminal) {
-      $http.delete("http://localhost:3000/criminals/"+criminal._id)
-      .then(function(res) {
-        var index = self.all.indexOf(criminal);
-        self.all.splice(index, 1);
-      })
-    }
+app.controller("CriminalsController", function($scope, Criminal) {
+  var self = this;
+  self.all = [];
 
-    function updateCriminal(criminal) {
-      $http.update("http://localhost:3000/criminals/"+criminal._id)
-        .then(function(res){
-        // add things to update //
-        // will need to add a button //  
-      })
-    }
+  var allCriminals = Criminal.query(function(){
+    self.all = allCriminals.criminals;
+  })
 
-    function getCriminals() {
-      $http.get("http://localhost:3000/criminals")
-      .then(function(res){
-        self.all = res.data.criminals;
-      })
-    }
-    getCriminals();
-  }
+  var entry = Entry.get({ id: $scope.id}, function(){
+    console.log('entry')
+  })
+});
+
+  // function CriminalsController() {
+  //   resources :criminals
+  //   // function addCriminal() {
+  //   //   $http.post("http://localhost:3000/criminals", self.newCriminal)
+  //   //     .then(function(data){
+  //   //       self.all = newCriminal.data.criminals;
+  //   //     })
+  //   //     self.all.push(this.newCriminal);
+  //   //     self.newCriminal = {};
+  //   // }
+  //   //
+  //   // function deleteCriminal(criminal) {
+  //   //   $http.delete("http://localhost:3000/criminals/"+criminal._id)
+  //   //   .then(function(res) {
+  //   //     var index = self.all.indexOf(criminal);
+  //   //     self.all.splice(index, 1);
+  //   //   })
+  //   // }
+  //   //
+  //   // function updateCriminal(criminal) {
+  //   //   $http.update("http://localhost:3000/criminals/"+criminal._id)
+  //   //     .then(function(res){
+  //   //     // add things to update //
+  //   //     // will need to add a button //
+  //   //   })
+  //   // }
+  //   //
+  //   // function getCriminals() {
+  //   //   $http.get("http://localhost:3000/criminals")
+  //   //   .then(function(res){
+  //   //     self.all = res.data.criminals;
+  //   //   })
+  //   // }
+  //   // getCriminals();
+  // }
